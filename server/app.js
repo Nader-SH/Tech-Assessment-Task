4f3e7fa0180694a2d87c5317ed02137c8602deb3
Nader-SH <nadershak149@gmail.com>
2023-08-01 17:00:33 +0300

    file-structure-connection-database

---
+++ server/app.js
@@ -0,0 +1,43 @@
+import express from "express";
+import compression from "compression";
+import cookieParser from "cookie-parser";
+import * as dotenv from "dotenv";
+import { join } from "path";
+// import router from "./routes/index.js";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+
+const { NODE_ENV } = process.env;
+
+app.set("port", process.env.PORT || 3001);
+
+app.use([
+  compression(),
+  cookieParser(),
+  express.urlencoded({ extended: false }),
+  express.json(),
+  cors({
+    origin: "*",
+  }),
+]);
+
+// app.use("/api/v1", router);
+
+if (NODE_ENV === "production") {
+  app.use(express.static(join(__dirname, "..", "client", "build")));
+  app.get("*", (req, res) => {
+    res.sendFile(join(__dirname, "..", "client", "build", "index.html"));
+  });
+}
+app.use((req, res, next) => res.status(404).json({ error: "Not Found" }));
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json(err.message);
+});
+
+export default app;
---
+++ server/models/user.js
@@ -0,0 +1,40 @@
+import { Model, DataTypes } from 'sequelize';
+import sequelize from "../database/config/connection.js";
+
+export default class User extends Model {
+  id;
+  first_name;
+  last_name
+  email;
+  password;
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    last_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  { sequelize }
+);
